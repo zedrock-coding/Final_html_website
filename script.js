6b359c98a25b14cba56106e0a04f5e9b83f9b8b6
Lakshya Jadhav <lakshyajadhav15@gmail.com>
2026-04-20 16:42:35 +0530

    image carousel complete

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-const slideshow = document.querySelectorAll('[data-slide-container]');
-const slidedistance = window.screen.width / 1.5;
+const slideshow = document.querySelectorAll('.slideshow-container');
+const slideDistance = window.screen.width / 1.5;
 
 slideshow.forEach((slide) => {
-    let containerMotion = slide.parentElement.attributes['data-slide-container'].value;
+    let containerMotion = slide.parentElement.getAttribute('data-slide-motion');
 
     if (containerMotion == "slide") {
       setInterval(function () {
-        container.scrollLeft += slideDistance;
+        slide.scrollLeft += slideDistance;
       }, 5000);
     }
 });
